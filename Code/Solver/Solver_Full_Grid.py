568d7523cbd306b9bca548a3ea621b057e3ced78
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2019-12-28 16:41:06 -0500

    updated solver to the sigma critical

--- Code/Solver/Solver_Full_Grid.py
+++ Code/Solver/Solver_Full_Grid.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 File Name: Solver_Full_Grid.py
-Purpose: Solves the equation of motion for full grid method
+Purpose: Solve the equation of motion for full grid method
 Author: Samuel Wong
 """
 import sys
@@ -9,19 +9,19 @@ sys.path.append("../Tools")
 import numpy as np
 import matplotlib.pyplot as plt
 from Grid import Standard_Dipole_Grid
-from Charge import Charge
+from Sigma_Critical import Sigma_Critical
 from Relaxation import Relaxation
-from Math import Superpotential
 
-def Solver_Full_Grid(N,charge_arg,L,w,h,R,tol,max_loop,x0=None,diagnose=False):
-    charge = Charge(N,charge_arg)
+def Solver_Full_Grid(N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,x0=None,
+                     diagnose=False):
+    charge = Sigma_Critical(N,charge_arg)
+    bound = Sigma_Critical(N,bound_arg)
     grid = Standard_Dipole_Grid(L,w,h,R)
-    W = Superpotential(N)
-    bound = W.x_min[0,:]
-    relax = Relaxation(grid,N,bound,charge.vector,tol,max_loop,x0,diagnose)
+    relax = Relaxation(grid,N,bound.imaginary_vector,charge.real_vector,tol,
+                       max_loop,x0,diagnose)
     relax.solve()
     x = relax.x
     return x
 
 if __name__ == "__main__":
-    x = Solver_Full_Grid(2,"w1",5,5,0.05,1,1E-30,500,diagnose = True)
+    x = Solver_Full_Grid(2,"w1",5,5,0.05,1,1E-30,500,diagnose=True)
