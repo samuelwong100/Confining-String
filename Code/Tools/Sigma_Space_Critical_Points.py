c248195acdaab8ef38ff4dbcdef84fde0ca1fb18
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2019-12-28 13:17:45 -0500

    Added the capability of storing sigma as real or imaginary

--- Code/Tools/Sigma_Space_Critical_Points.py
+++ Code/Tools/Sigma_Space_Critical_Points.py
@@ -15,6 +15,15 @@ class Sigma_Critical():
     comdinations of fundamental weights and field vaccua, which can be sum of 
     x_min vector with fundamental weights.
     
+    Warning: There is a problem of convention. Technically, the sigma space is
+    the imaginary component of the complex field, so sigma itself is real. When
+    dealing with charge, we often talk about fundamental weights, which are 
+    taken to be a real vector. But when we talk about x_min, it is usually in
+    a context of it being the vacua of a full complex field, so it is often
+    taken to be a purely imaginary vector. Since we want to have a class that
+    deals with both generically, I will create two vectors, real and imaginary.
+    They only differ by a factor of i.
+    
     Variables
     ----------------------------------------
     N (int) = Number of colors
@@ -25,19 +34,20 @@ class Sigma_Critical():
                where the first letter of each term is either 'w' or 'x',
                followed by a integer (i,j,k), and it is either "+" or "-" for 
                each sign.
-    vector (array) = an array of shape (N-1,); the sum of fundamental weights
+    vector (array) = an array of shape (N-1,)
     """
     def __init__(self,N,arg):
         self.N = N
         self.S = SU(N)
         self.W = Superpotential(N)
         self.name = arg
-        self.vector = self._define_vector(arg)
+        self.real_vector = self._define_real_vector(arg)
+        self.imaginary_vector = 1j*self.real_vector
             
     def __str__(self):
         return self.name
     
-    def _define_vector(self,arg):
+    def _define_real_vector(self,arg):
         str_list = arg.split() #split the string into list; separator is space
         return self._sum_up_terms(str_list)
             
@@ -48,9 +58,12 @@ class Sigma_Critical():
             if len(term)==2:
                 k = int(term[1])
                 if term[0] == 'w':
-                    summation += sign*self.S.w[k-1,:]
+                    #the usual w is already real
+                    summation += sign*np.real(self.S.w[k-1,:])
                 elif term[0] == 'x':
-                    summation += sign*self.W.x_min[k,:]
+                    #the usual x_min is stored as purely imaginary
+                    #want to convert to a real vector
+                    summation += sign*np.imag(self.W.x_min[k,:])
                 else:
                     raise Exception("Unacceptable sigma crticial points \
                                     specification.")
