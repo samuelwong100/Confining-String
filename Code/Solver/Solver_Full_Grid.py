31b089bf080e94f6bfc6a181010712b03f9a0083
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2019-12-30 09:44:16 -0500

    added storage function

--- Code/Solver/Solver_Full_Grid.py
+++ Code/Solver/Solver_Full_Grid.py
@@ -11,17 +11,27 @@ import matplotlib.pyplot as plt
 from Grid import Standard_Dipole_Grid
 from Sigma_Critical import Sigma_Critical
 from Relaxation import Relaxation
+from Solver_Helpers import get_title, store_solution
 
 def Solver_Full_Grid(N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,x0=None,
                      diagnose=False):
-    charge = Sigma_Critical(N,charge_arg)
-    bound = Sigma_Critical(N,bound_arg)
-    grid = Standard_Dipole_Grid(L,w,h,R)
-    relax = Relaxation(grid,N,bound.imaginary_vector,charge.real_vector,tol,
-                       max_loop,x0,diagnose)
-    relax.solve()
-    x = relax.x
-    return x
+    prefix = "../Results/Solutions/"
+    title = get_title(prefix,N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,x0,
+                      "full grid")
+    if sys.path.exists(title):
+        sol = Solution_Viewer(title)
+    else:
+        charge = Sigma_Critical(N,charge_arg)
+        bound = Sigma_Critical(N,bound_arg)
+        grid = Standard_Dipole_Grid(L,w,h,R)
+        relax = Relaxation(grid,N,bound.imaginary_vector,charge.real_vector,
+                           tol,max_loop,x0,diagnose)
+        relax.solve()
+        store_solution(relax,title,N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,
+                       x0,"full grid")
+        
+        sol = Solution_Viewer(title,[grid,relax.x,relax.error,relax.loop])
+    sol.display_all()
 
 if __name__ == "__main__":
     x = Solver_Full_Grid(N=2,charge_arg="w1",bound_arg="x0",L=5,w=5,h=0.05,R=1,
--- Code/Tools/Solver_Helpers.py
+++ Code/Tools/Solver_Helpers.py
@@ -4,9 +4,26 @@ File Name: Solver_Helpers.py
 Purpose: Helper functions for solver
 Author: Samuel Wong
 """
+import pickle
 
-def get_title(N,charge,bound,L,w,h,R,tol,max_loop,x0,method):
-    title = "CS(N={}, charge={}, bound={}, L={}, w={}, h={},\
-    R={}, tol={}, max_loop={}, x0={}, method={})".format(str(N),charge,bound,
-    str(L),str(w),str(h),str(R),str(tol),str(max_loop),str(x0),method)
+def get_title(prefix,N,charge,bound,L,w,h,R,tol,max_loop,x0,method):
+    title = "{}CS(N={}, charge={}, bound={}, L={}, w={}, h={},\
+    R={}, tol={}, max_loop={}, x0={}, method={}/)".format(prefix,str(N),charge,
+    bound,str(L),str(w),str(h),str(R),str(tol),str(max_loop),str(x0),method)
     return title
+
+def store_solution(relax,title,N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,x0,
+                   method):
+    #store the core result in a dictionary
+    core_dict = {"N":N,"charge_arg":charge_arg,"bound_arg":bound_arg,"L":L,
+                  "w":w,"h":h,"R":R,"tol":tol,"max_loop":max_loop,"x0":x0,
+                  "method":method,"loop":relax.loop,"field":relax.x,
+                  "error":relax.error}
+    pickle_out = open(title+"core_dict.pickle","wb")
+    pickle.dump(core_dict, pickle_out)
+    pickle_out.close()
+    #code for opening file
+#    pickle_in = open(title+"core_dict.pickle","rb")
+#    core_dict = pickle.load(pickle_in)
+    
+    
