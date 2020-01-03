27a470742e2de64fa5599ba44ae819021044d133
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-01-03 14:11:53 -0500

    Update Solver_Helpers.py

--- Code/Tools/Solver_Helpers.py
+++ Code/Tools/Solver_Helpers.py
@@ -4,11 +4,13 @@ File Name: Solver_Helpers.py
 Purpose: Helper functions for solver
 Author: Samuel Wong
 """
+import os
 import pickle
 
 def get_title(prefix,N,charge,bound,L,w,h,R,tol,max_loop,x0,method):
-    title = "{}CS(N={}, charge={}, bound={}, L={}, w={}, h={},\
-    R={}, tol={}, max_loop={}, x0={}, method={}/)".format(prefix,str(N),charge,
+    title =\
+    ('{}CS(N={},charge={},bound={},L={},w={},h={},R={},tol={},'+ \
+    'max_loop={},x0={},method={})/').format(prefix,str(N),charge,
     bound,str(L),str(w),str(h),str(R),str(tol),str(max_loop),str(x0),method)
     return title
 
@@ -19,10 +21,9 @@ def store_solution(relax,title,N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,x0,
                   "w":w,"h":h,"R":R,"tol":tol,"max_loop":max_loop,"x0":x0,
                   "method":method,"loop":relax.loop,"field":relax.x,
                   "error":relax.error}
-    pickle_out = open(title+"core_dict.pickle","wb")
-    pickle.dump(core_dict, pickle_out)
-    pickle_out.close()
-    #code for opening file
-#    pickle_in = open(title+"core_dict.pickle","rb")
-#    core_dict = pickle.load(pickle_in)
-    
+    #create directory for new folder if it doesn't exist
+    if not os.path.exists(title):
+        os.makedirs(title)
+    #dump dictionary into pickle
+    with open(title+"core_dict","wb") as file:
+        pickle.dump(core_dict, file)
