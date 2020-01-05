c60d9b3b2c5abdbf9ce389dc7981e4d691923829
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-01-04 21:53:00 -0500

    reordered functions

--- Code/Solver/Solution_Viewer.py
+++ Code/Solver/Solution_Viewer.py
@@ -113,14 +113,6 @@ class Solution_Viewer():
         for n in range(self.m):
             self.plot_phi_n(n)
             self.plot_sigma_n(n)
-    
-    def _quick_plot(self,field,title,file_title):
-        plt.figure()
-        plt.pcolormesh(self.grid.zv,self.grid.yv,field)
-        plt.colorbar()
-        plt.title(title)
-        plt.savefig(self.folder_title+file_title+".png")
-        plt.show()
 
     def plot_error(self):
         """
@@ -142,30 +134,6 @@ class Solution_Viewer():
                     d2xdz[i][j][k] = self._get_d2xdz_ijk(i,j,k)
                     d2xdy[i][j][k] = self._get_d2xdy_ijk(i,j,k)
         return d2xdz + d2xdy
-        
-    def _get_d2xdz_ijk(self,i,j,k):
-        if k == 0: #one sided second derivative on the edge (forward difference)
-            result = (self.x[i][j][k+2] - 2*self.x[i][j][k+1] +
-                      self.x[i][j][k])/(self.grid.h**2)
-        elif k==self.grid.num_z-1: #one sided second derivative on the edge
-            result = (self.x[i][j][k] - 2*self.x[i][j][k-1] +
-                      self.x[i][j][k-2])/(self.grid.h**2)
-        else: #two sided second derivative elsewhere
-            result = (self.x[i][j][k+1] - 2*self.x[i][j][k] +
-                      self.x[i][j][k-1])/(self.grid.h**2)
-        return result
-    
-    def _get_d2xdy_ijk(self,i,j,k):
-        if j == 0: #one sided derivative on the edge
-            result = (self.x[i][j+2][k] - 2*self.x[i][j+1][k] +
-                      self.x[i][j][k])/(self.grid.h**2)
-        elif j==self.grid.num_y-1: #one sided derivative on the edge
-            result = (self.x[i][j][k] - 2*self.x[i][j-1][k] +
-                      self.x[i][j-2][k])/(self.grid.h**2)
-        else: #two sided derivative elsewhere
-            result = (self.x[i][j+1][k] - 2*self.x[i][j][k] +
-                      self.x[i][j-1][k])/(self.grid.h**2)
-        return result
 
     def plot_laplacian_all(self):
         """
@@ -176,45 +144,6 @@ class Solution_Viewer():
         lap_theo = self._get_lap_theo()
         for n in range(self.m):
             self._plot_laplacian_n(n,lap_num,lap_theo)
-            
-    def _plot_laplacian_n(self,n,lap_num,lap_theo):
-        #row= real & imag of fields; col= numeric vs theoretic
-        fig, axs = plt.subplots(2, 2) 
-        fig.subplots_adjust(hspace=0.7)
-        fig.subplots_adjust(wspace=0.7)
-        self._quick_plot_laplacian(np.real(lap_num[n,:,:]),axs[0, 0],
-                        "$\\nabla^2 \phi_{}$ numeric".format(str(n+1)),
-                                   fig)
-        self._quick_plot_laplacian(np.real(lap_theo[n,:,:]),axs[0,1],
-                        "$\\nabla^2 \phi_{}$ theoretic".format(str(n+1)),
-                                   fig)
-        self._quick_plot_laplacian(np.imag(lap_num[n,:,:]),axs[1, 0],
-                        "$\\nabla^2 \sigma_{}$ numeric".format(str(n+1)),
-                                   fig)
-        self._quick_plot_laplacian(np.imag(lap_theo[n,:,:]),axs[1,1],
-                        "$\\nabla^2 \sigma_{}$ theoretic".format(str(n+1)),
-                            fig)
-        #add axis label such that repeated are avoided
-        #for ax in axs.flat:
-            #ax.set(xlabel='z', ylabel='y')
-        # Hide x labels and tick labels for top plots and y ticks for right plots.
-        #for ax in axs.flat:
-            #ax.label_outer()
-        fig.savefig(self.folder_title+"Laplacian_{}.png".format(str(n+1)))
-        
-    def _quick_plot_laplacian(self,field,ax,title,fig):
-        im = ax.pcolormesh(self.grid.zv,self.grid.yv,field)
-        ax.set_title(title)
-        fig.colorbar(im,ax=ax)
-            
-    def _get_lap_theo(self):
-        #return theoretical laplacian
-        charge = Sigma_Critical(self.N,self.charge_arg)
-        bound = Sigma_Critical(self.N,self.bound_arg)
-        relax = Relaxation(self.grid,self.N,bound.imaginary_vector,
-                           charge.real_vector,self.tol,self.max_loop,x0=None,
-                           diagnose=False)
-        return relax._full_grid_EOM(self.x)
     
     def get_gradient_energy_density(self):
         """
@@ -255,6 +184,53 @@ class Solution_Viewer():
         plt.savefig(self.folder_title+"Gradient_Energy_Density.png")
         plt.show()
     
+    def _quick_plot(self,field,title,file_title):
+        plt.figure()
+        plt.pcolormesh(self.grid.zv,self.grid.yv,field)
+        plt.colorbar()
+        plt.title(title)
+        plt.savefig(self.folder_title+file_title+".png")
+        plt.show()
+        
+    def _quick_plot_laplacian(self,field,ax,title,fig):
+        im = ax.pcolormesh(self.grid.zv,self.grid.yv,field)
+        ax.set_title(title)
+        fig.colorbar(im,ax=ax)
+
+    def _plot_laplacian_n(self,n,lap_num,lap_theo):
+        #row= real & imag of fields; col= numeric vs theoretic
+        fig, axs = plt.subplots(2, 2) 
+        fig.subplots_adjust(hspace=0.7)
+        fig.subplots_adjust(wspace=0.7)
+        self._quick_plot_laplacian(np.real(lap_num[n,:,:]),axs[0, 0],
+                        "$\\nabla^2 \phi_{}$ numeric".format(str(n+1)),
+                                   fig)
+        self._quick_plot_laplacian(np.real(lap_theo[n,:,:]),axs[0,1],
+                        "$\\nabla^2 \phi_{}$ theoretic".format(str(n+1)),
+                                   fig)
+        self._quick_plot_laplacian(np.imag(lap_num[n,:,:]),axs[1, 0],
+                        "$\\nabla^2 \sigma_{}$ numeric".format(str(n+1)),
+                                   fig)
+        self._quick_plot_laplacian(np.imag(lap_theo[n,:,:]),axs[1,1],
+                        "$\\nabla^2 \sigma_{}$ theoretic".format(str(n+1)),
+                            fig)
+        #add axis label such that repeated are avoided
+        #for ax in axs.flat:
+            #ax.set(xlabel='z', ylabel='y')
+        # Hide x labels and tick labels for top plots and y ticks for right plots.
+        #for ax in axs.flat:
+            #ax.label_outer()
+        fig.savefig(self.folder_title+"Laplacian_{}.png".format(str(n+1)))
+            
+    def _get_lap_theo(self):
+        #return theoretical laplacian
+        charge = Sigma_Critical(self.N,self.charge_arg)
+        bound = Sigma_Critical(self.N,self.bound_arg)
+        relax = Relaxation(self.grid,self.N,bound.imaginary_vector,
+                           charge.real_vector,self.tol,self.max_loop,x0=None,
+                           diagnose=False)
+        return relax._full_grid_EOM(self.x)
+    
     def _get_derivative(self):
         #initialize derivative in each direction
         dxdz = np.zeros(shape=self.x.shape,dtype=complex)
@@ -288,6 +264,53 @@ class Solution_Viewer():
         else: #two sided derivative elsewhere
             result = (self.x[i][j+1][k] - self.x[i][j-1][k])/(2*self.grid.h)
         return result
+    
+    def _get_d2xdz_ijk(self,i,j,k):
+        if k == 0: #one sided second derivative on the edge (forward difference)
+            result = (self.x[i][j][k+2] - 2*self.x[i][j][k+1] +
+                      self.x[i][j][k])/(self.grid.h**2)
+        elif k==self.grid.num_z-1: #one sided second derivative on the edge
+            result = (self.x[i][j][k] - 2*self.x[i][j][k-1] +
+                      self.x[i][j][k-2])/(self.grid.h**2)
+        else: #two sided second derivative elsewhere
+            result = (self.x[i][j][k+1] - 2*self.x[i][j][k] +
+                      self.x[i][j][k-1])/(self.grid.h**2)
+        return result
+    
+    def _get_d2xdy_ijk(self,i,j,k):
+        if j == 0: #one sided derivative on the edge
+            result = (self.x[i][j+2][k] - 2*self.x[i][j+1][k] +
+                      self.x[i][j][k])/(self.grid.h**2)
+        elif j==self.grid.num_y-1: #one sided derivative on the edge
+            result = (self.x[i][j][k] - 2*self.x[i][j-1][k] +
+                      self.x[i][j-2][k])/(self.grid.h**2)
+        else: #two sided derivative elsewhere
+            result = (self.x[i][j+1][k] - 2*self.x[i][j][k] +
+                      self.x[i][j-1][k])/(self.grid.h**2)
+        return result
+    
+#    def get_potential_energy_density(self):
+#        W = Superpotential(self.N)
+#        ped = (1/4)*W.dWdx_absolute_square_on_grid(self.x)
+#        ped = np.real(ped) #it is real anyway
+#        return ped
+#               
+#    def get_potential_energy(self):
+#        return simps(simps(self.get_potential_energy_density(),self.grid.z),self.grid.y)
+#    
+#    def plot_potential_energy_density(self,save=False):
+#        """
+#        Plot the potential energy density.
+#        """
+#        plt.figure()
+#        plt.pcolormesh(self.grid.zv,self.grid.yv,
+#                       self.get_potential_energy_density(),cmap='jet')
+#        plt.colorbar()
+#        #plt.title("Potential Energy Density")
+#        if save:
+#            plt.savefig(self.title+" Potential Energy Density.png")
+#        plt.show()
+#
 
 #class Solution_Viewer0():
 #    """
@@ -415,28 +438,7 @@ class Solution_Viewer():
 #            result = (self.x[i][j][k+1] - self.x[i][j][k-1])/(2*self.grid.h)
 #        return result
 #    
-#    def get_potential_energy_density(self):
-#        W = Superpotential(self.N)
-#        ped = (1/4)*W.dWdx_absolute_square_on_grid(self.x)
-#        ped = np.real(ped) #it is real anyway
-#        return ped
-#               
-#    def get_potential_energy(self):
-#        return simps(simps(self.get_potential_energy_density(),self.grid.z),self.grid.y)
-#    
-#    def plot_potential_energy_density(self,save=False):
-#        """
-#        Plot the potential energy density.
-#        """
-#        plt.figure()
-#        plt.pcolormesh(self.grid.zv,self.grid.yv,
-#                       self.get_potential_energy_density(),cmap='jet')
-#        plt.colorbar()
-#        #plt.title("Potential Energy Density")
-#        if save:
-#            plt.savefig(self.title+" Potential Energy Density.png")
-#        plt.show()
-#
+
 #    def get_energy_density(self):
 #        return self.get_potential_energy_density() \
 #               + self.get_gradient_energy_density()
