38139adafbd7b1ec1977628c4e186260977c75cd
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-01-29 16:59:57 -0500

    program the main continue solving function; just need a new relaxation

--- Code/Solver/Solver_Full_Grid.py
+++ Code/Solver/Solver_Full_Grid.py
@@ -31,3 +31,41 @@ def Solver_Full_Grid(N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,x0="BPS",
         sol = Solution_Viewer(title)
     sol.display_all()
     return sol
+
+def Continue_Solving_Full_Grid(old_title,max_loop,diagnose=True):
+    new_title = _get_new_from_old_title(old_title,max_loop)
+    #get old title into a format used by solution viewer
+    old_title = "../Results/Solutions/" + old_title + "/"
+    old_sol = Solution_Viewer(old_title)
+    #get the parameters from the old solution
+    N = old_sol.N
+    tol = old_sol.tol
+    x0 = old_sol.x0
+    charge_arg = old_sol.charge_arg
+    bound_arg = old_sol.bound_arg
+    L = old_sol.L
+    w = old_sol.w
+    h = old_sol.h
+    R = old_sol.R
+    #create a continuing relaxation object, which starts with old field
+    relax = Continue_Relaxation(old_sol, max_loop, diagnose)
+    relax.solve()
+    store_solution(relax,new_title,N,charge_arg,bound_arg,L,w,h,R,tol,max_loop,
+                   x0,"full grid")
+    sol = Solution_Viewer(new_title)
+    sol.display_all()
+    return sol
+
+def _get_new_from_old_title(old_title,max_loop):
+    #the index of the letter 'm' in 'max_loop='
+    index_m = old_title.find("max_loop=")
+    index_equal_sign = index_m + 8
+    #get the index of the comma immediately after the max loop number
+    index_comma = old_title.find(",",index_m) #start search at index_m
+    #get the parts before and after the loop number
+    first_part = old_title[0:index_equal_sign+1]
+    last_part = old_title[index_comma,len(old_title)]
+    #insert the new max_loop
+    new_title = first_part+str(max_loop)+last_part
+    return new_title
+    
