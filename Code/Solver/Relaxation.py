bcc6e8320792ddd21210294e8d2a9b738bc07e5b
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-02-10 02:06:13 -0500

    small change

--- Code/Solver/Relaxation.py
+++ Code/Solver/Relaxation.py
@@ -294,9 +294,8 @@ class Continue_Relaxation(Relaxation):
         return self.x
     
 class Relaxation_half_grid(Relaxation):
-    def __init__(self,grid,N,bound,charge,tol,max_loop,x0,diagnose):
-        self.full_grid = grid
-        self.grid = Half_Grid(grid)
+    def __init__(self,full_grid,N,bound,charge,tol,max_loop,x0,diagnose):
+        self.grid = Half_Grid(full_grid)
         #need to explicitly use the half grid in superclass construction
         super().__init__(self.grid,N,bound,charge,tol,max_loop,x0,diagnose)
 
