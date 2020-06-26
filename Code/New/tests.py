5147268a6dbfce4a67082fc39e90777009e20567
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-06-26 01:32:42 -0400

    Update tests.py

--- Code/New/tests.py
+++ Code/New/tests.py
@@ -5,9 +5,9 @@ Purpose:
 Author: Samuel Wong
 """
 import numpy as np
-from source import Dipole_Full_Grid
-from source import within_epsilon
-
+from source import Dipole_Full_Grid, within_epsilon, relaxation_update_full_grid,\
+relaxation_update_half_grid, relaxation_algorithm
+import matplotlib.pyplot as plt
 def test_dipole_full_grid():
     DFG = Dipole_Full_Grid(11,7,3)
     hg = DFG.half_grid
@@ -54,5 +54,80 @@ def test_dipole_full_grid():
     y[:,:,0] = 1
     x_new = hg2.reflect_vector_field(y)
     assert within_epsilon(np.max(x-x_new),0)
+
+def laplacian_function(x):
+    return 2*x
+
+def test_relaxation_update():
+    grid = Dipole_Full_Grid(3,3,1)
+    x_old =  np.array([[[1.,2,3],[4,5,6],[7,8,9]],
+                       [[1,2,3],[4,5,6],[7,8,9]],
+                       [[1,2,3],[4,5,6],[7,8,9]]])
+    x = relaxation_update_full_grid(x_old=x_old,laplacian_function=laplacian_function,
+                             grid=grid)
+    x_correct = np.array([[[1,2,3],[4,4.975,6],[7,8,9]],
+                       [[1,2,3],[4,4.975,6],[7,8,9]],
+                       [[1,2,3],[4,4.975,6],[7,8,9]]])
+    assert within_epsilon(x,x_correct)
+    
+    y = relaxation_update_half_grid(x_old=x_old,laplacian_function=laplacian_function,
+                             grid=grid)
+    y_correct = np.array([[[1,2,2],[4,4.975,4.975],[7,8,8]],
+                       [[1,2,2],[4,4.975,4.975],[7,8,8]],
+                       [[1,2,2],[4,4.975,4.975],[7,8,8]]])
+    assert within_epsilon(y,y_correct)
+    
+def test_half_grid_reflect():
+    grid = Dipole_Full_Grid(3,3,1)
+    y = np.array([[[1,2,2],[4,4.975,4.975],[7,8,8]],
+                       [[1,2,2],[4,4.975,4.975],[7,8,8]],
+                       [[1,2,2],[4,4.975,4.975],[7,8,8]]])
+    y_flipped = np.array([[[1.,2.,2.,2.,1.],
+                              [4.,4.975,4.975,4.975,4.],
+                              [7.,8.,8.,8.,7.,]],
+                                [[1.,2.,2.,2.,1.],
+                              [4.,4.975,4.975,4.975,4.],
+                              [7.,8.,8.,8.,7.,]],
+                                [[1.,2.,2.,2.,1.],
+                              [4.,4.975,4.975,4.975,4.],
+                              [7.,8.,8.,8.,7.,]]])
+    assert within_epsilon(grid.half_grid.reflect_vector_field(y),y_flipped)
+    
+def test_relaxation_algorithm():
+    #the boundary value problem: laplacian(f) = e^(|x|), boundary =e^(bounday),
+    # is solved by f = e^(|x|)        
+    DFG = Dipole_Full_Grid(31,31,13)
+    def exponential_laplacian(x):
+        result = DFG.create_vector_field(x.shape[0],dtype=float)
+        for m in range(x.shape[0]):
+            for row in range(x.shape[1]):
+                result[m][row] = np.exp(np.abs(DFG.z_linspace))
+        return result
+    x_initial = DFG.create_vector_field(2,dtype=float)
+    x_correct=exponential_laplacian(x_initial)
+    x_initial[:,:,0] =x_correct[:,:,0]
+    x_initial[:,:,-1] =x_correct[:,:,-1]
+    x_initial[:,0,:] =x_correct[:,0,:]
+    x_initial[:,-1,:] =x_correct[:,-1,:]
+    x,error,loop = relaxation_algorithm(x_correct, exponential_laplacian,
+                         DFG,tol=1e-9,use_half_grid=True)
+    for i in range(2):
+        plt.figure()
+        plt.pcolormesh(DFG.zv,DFG.yv,x_correct[i,:,:])
+        plt.colorbar()
+        plt.title("Correct solution: f(x,y) = e^-x")
+        plt.show()
+        
+    for i in range(2):
+        plt.figure()
+        plt.pcolormesh(DFG.zv,DFG.yv,x[i,:,:])
+        plt.colorbar()
+        plt.title("my solution")
+        plt.show()
+    return x, error,loop
+    
     
 test_dipole_full_grid()
+test_relaxation_update()
+test_half_grid_reflect()
+#x, error,loop = test_relaxation_algorithm()
