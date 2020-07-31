0cb2ec808714267e2f8b7bafa63fdae087658d86
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-07-31 12:06:46 -0400

    Create trial_run_script.sh

---
+++ Niagara/trial_run_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=80 #could be 40
+#SBATCH --time=1:00:00
+#SBATCH	--job-name=CS_k_1
+#SBATCH --mail-user=samuelsy.wong@mail.utoronto.ca
+#SBATCH --mail-type=ALL #email you everything
+
+cd $SLURM_SUBMIT_DIR
+module restore confine_module #python, gnu parrallel
+source ~/.virtualenvs/confine_envs/bin/activate #confinepy
+
+timeout 58m parallel --joblog "$SLURM_JOBID.log" -j $SLURM_TASKS_PER_NODE "python trial_run.py {1} {2} {3}" ::: {2..9} ::: 1 ::: $(seq 50 -2 4)
+EXIT=$?
+
+if [ $EXIT -eq 124 ]
+then
+    ssh -t nia-login01 "cd $SLURM_SUBMIT_DIR; sbatch trial_run_script.sh"
+fi
