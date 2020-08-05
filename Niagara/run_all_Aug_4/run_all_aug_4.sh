e5c54105cd25f535934501cc49ad02c525b12023
Samuel Wong <samuelsy.wong@mail.utoronto.ca>
2020-08-05 02:11:08 -0400

    Update run_all_aug_4.sh

--- Niagara/run_all_Aug_4/run_all_aug_4.sh
+++ Niagara/run_all_Aug_4/run_all_aug_4.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=80 #could be 40
 #SBATCH --time=1:00:00
-#SBATCH	--job-name=CS_k_1
+#SBATCH	--job-name=CS_all
 #SBATCH --mail-user=samuelsy.wong@mail.utoronto.ca
 #SBATCH --mail-type=ALL #email you everything
 
@@ -10,10 +10,10 @@ cd $SLURM_SUBMIT_DIR
 module restore confine_module #python, gnu parrallel
 source ~/.virtualenvs/confine_envs/bin/activate #confinepy
 
-timeout 58m parallel --joblog "$SLURM_JOBID.log" -j $SLURM_TASKS_PER_NODE "python trial_run.py {1} {2} {3}" ::: {2..9} ::: {2..4} ::: $(seq 50 -5 5)
+timeout 58m parallel --joblog "$SLURM_JOBID.log" -j $SLURM_TASKS_PER_NODE "python main_script.py {1} {2} {3}" ::: {4..9} ::: {2..4} ::: $(seq 50 -5 5)
 EXIT=$?
 
 if [ $EXIT -eq 124 ]
 then
-    ssh -t nia-login01 "cd $SLURM_SUBMIT_DIR; sbatch trial_run_script.sh"
+    ssh -t nia-login01 "cd $SLURM_SUBMIT_DIR; sbatch run_all_aug_4.sh"
 fi
